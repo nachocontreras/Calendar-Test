aa3dbb1d1bd5b7cb6a8cb04d90dff077bbf56b5d
Ignacio Contreras <icontreras1@uc.cl>
2017-12-21 14:51:08 -0300

    Manage list events with categories and users

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 from init_variables import get_service
+from config import user_key, categories
+from collections import defaultdict
 
 def get_events_list(calendar_id):
     results = {}
@@ -14,8 +16,18 @@ def get_events_list(calendar_id):
     results = {}
 
 def manage_list_events():
+    final = defaultdict(int)
     calendar_id = input("Ingrese mail de calendario")
     events = get_events_list(calendar_id)
     for name, dates in events.items():
         print(name, date)
-    
+        for cat in categories:
+            if cat in name:
+                for user in user_key:
+                    if user in name:
+                        final[user_key[user]] += categories[cat]
+                        break
+                break
+        print(final)
+    return final
+        
